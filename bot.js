4a73f1dd123d4c6be948efc8dbea6d7095720652
iishutov <ilya11ishutov@gmail.com>
2023-01-16 23:15:33 +0700

    add Fisher–Yates shuffle for goal message

--- bot.js
+++ bot.js
@@ -7,6 +7,12 @@ const token = '**********************************************'
 const bot = new TelegramBot(token, { polling: true })
 process.env["NTBA_FIX_350"] = 1
 
+/*const db = new sqlite3.Database('./sql.db', () => {
+    db.run(`CREATE TABLE IF NOT EXISTS goals_count (goals INTEGER NOT NULL)`, () => {
+        db.run(`INSERT INTO goals_count (goals) VALUES (?)`, [741])
+    })
+})*/
+
 const link = 'https://www.sports.ru/messi/career/'
 let loggedInUsers = new Set()
 let goals
@@ -15,15 +21,12 @@ const db = new sqlite3.Database('./sql.db', () => {
 		goals = row.goals
 	})
 })
+shuffle(goalStrList)
+shuffle(emojiList)
 
-/*const db = new sqlite3.Database('./sql.db', () => {
-    db.run(`CREATE TABLE IF NOT EXISTS goals_count (goals INTEGER NOT NULL);`, () => {
-        db.run(`INSERT INTO goals_count (goals) VALUES (?)`, [741])
-    })
-})*/
-
-let i = 1
+let requestNum = 1
 setInterval(checkForGoal, 60 * 1000)
+
 
 bot.onText(/\/start/, async (msg) => {
 	await bot.sendPhoto(msg.chat.id, './Messimania.jpg')
@@ -37,7 +40,7 @@ bot.onText(/\/goals_count/, async (msg) => {
 })
 
 async function checkForGoal(){
-    console.log(`Started page request №${i++}...`)
+    console.log(`Started page request №${requestNum++}...`)
     try {
         await axios.get(link).then(async (resp) => {
             const doc = new JSDOM(resp.data)
@@ -49,12 +52,15 @@ async function checkForGoal(){
                                                     innerHTML.replace(/\n/g, '')
                 if (goals != currGoals){
                     console.log(`GOAL!\ngoals in db: ${goals},\ngoals count: ${currGoals}`)
+                    let goalAlarm = goalStrList[Math.floor(Math.random() * goalStrList.length)] +
+                                    emojiList[Math.floor(Math.random() * emojiList.length)]
                     loggedInUsers.forEach(userId => {
-                        goalAlarm = goalStr[Math.floor(Math.random()*goalStr.length)] + emoji[Math.floor(Math.random()*emoji.length)]
                         bot.sendMessage(userId, goalAlarm)
                     })
                     goals = currGoals
                     await updateGoals(goals)
+                    shuffle(goalStrList)
+                    shuffle(emojiList)
                 }
             }
         })
@@ -74,16 +80,24 @@ function updateGoals(currGoals){
     })
 }
 
+// Fisher–Yates shuffle.
+function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1))
+        [arr[i], arr[j]] = [arr[j], arr[i]]
+    }
+}
+
 let commandList = [
 	{ command: "/start", description: "Subscribe for new goals" },
 	{ command: "/goals_count", description: "Total goals by Messi" }
 ]
 
-let goalStr = [
+let goalStrList = [
 	'GOAL!', 'GOOAAL!!', 'GOAAL!!!',
 	'Goal!!!', 'GOLASSO!!', 'Goal Goal Goal!',
 	'Encara Messi! GOOOOAL!', 'Encara Messi Encara Messi! Golasso!'
 ]
-let emoji = [
+let emojiList = [
 	'⚽️', '😍', '😍','🥳','👽','👏','👑','🔥','💥',,'❤️','🔝'
 ]
